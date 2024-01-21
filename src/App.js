b6bd0447f55e66f1ff1d1faff3d82b862b2d3cf4
brianchum <bchum@students.cs.ubc.ca>
2024-01-20 22:30:46 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        var data = { message: message, email: email };
+
+        emailjs
+            .send(
+                'service_9v4oc7n',
+                'template_9b0aizb',
+                data,
+                'wCiYFTN_MgGRKiJn-'
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        setEmail('');
+        setMessage('');
+    };
+    return (
+        <div className='App'>
+            <form ref={form} onSubmit={sendEmail}>
+                <input
+                    type='email'
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                />
+                <input
+                    type='message'
+                    value={message}
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                    }}
+                />
+                <button type='submit'> Sumbit </button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
